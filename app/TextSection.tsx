13115fbc4a647fd7230a38a7c69c0e7a1818c88d
Ashraf Ali <ashrafali786522002@gmail.com.com>
2024-01-28 15:17:15 +0530

    All project done ✅

---
+++ app/TextSection.tsx
@@ -0,0 +1,82 @@
+"use client";
+
+import React from "react";
+import styled from "styled-components";
+import { motion, useScroll, useTransform } from "framer-motion";
+
+interface Props {
+  children: React.ReactNode;
+}
+
+const TextWrapper = ({ children }: Props) => {
+  const text = React.useRef<HTMLDivElement>(null);
+
+  const { scrollYProgress } = useScroll({
+    target: text,
+    offset: ["start end", "end start"],
+  });
+
+  const opacity = useTransform(scrollYProgress, [1, 0.8, 0], [1, 1, 0]);
+  const x = useTransform(scrollYProgress, [1, 0.4, 0], [0, 0, -1000]);
+  const colorChange = useTransform(
+    scrollYProgress,
+    [0, 0.2, 0.4, 0.6, 0.8, 1],
+    [
+      "hsla(180, 7%, 75%, 0.9)",
+      "hsla(180, 7%, 75%, 0.9)",
+      "#f2994a",
+      "#f2994a",
+      "#f2994a",
+      "#f2994a",
+    ]
+  );
+
+  return (
+    <div ref={text}>
+      <motion.p style={{ opacity, x, color: colorChange }}>{children}</motion.p>
+    </div>
+  );
+};
+
+function TextSection() {
+  return (
+    <TextSectionStyled>
+      <TextWrapper>
+        Lorem ipsum dolor, sit amet consectetur adipisicing.
+      </TextWrapper>
+      <TextWrapper>
+        Velit gravida euismod tortor, dapibus augue nulla et quam.
+      </TextWrapper>
+      <TextWrapper>
+        Amet consectetur adipiscing elit. Quisque ut augue non mauris.
+      </TextWrapper>
+      <TextWrapper>
+        Pulvinar fermentum ligula. Vestibulum ante ipsum primis in faucibus.
+      </TextWrapper>
+      <TextWrapper>
+        Fringilla id. Aliquam erat volutpat. Varius justo nulla tristique,
+        euismod..
+      </TextWrapper>
+      <TextWrapper>
+        Integer bibendum, libero nec interdum hendrerit, libero ligula aliquam
+        dui.
+      </TextWrapper>
+      <TextWrapper>
+        Mauris gravida eleifend. Nullam vitae justo vel erat pulvinar.
+      </TextWrapper>
+    </TextSectionStyled>
+  );
+}
+
+const TextSectionStyled = styled.section`
+  p {
+    font-size: 4rem;
+    padding-bottom: 1rem;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    gap: 3rem;
+  }
+`;
+
+export default TextSection;
